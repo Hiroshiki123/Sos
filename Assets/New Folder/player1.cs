40179e400496af387688cb3ebb318aa38c6eaa41
xFIREx2005 <henrigamerbr123043@gmail.com>
2022-08-10 12:17:04 -0300

    Sos1

---
+++ Assets/New Folder/player1.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1 : MonoBehaviour
+{
+    public float moveSpeed = 100f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    
+
+    private int vel;
+     void Update()
+    {
+        vel = 2;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+    
+    }
+
+
+    private void FixedUpdate()
+    {
+        vel = 2;
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            vel = 5;
+        }
+
+        rb.MovePosition(rb.position + movement * vel * Time.fixedDeltaTime);    }
+}
